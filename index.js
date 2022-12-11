96383e6c517cd908598c2e265b4597d4ceea8586
NXVZBGB FBEN <NXVZBGBFBEN@gmail.com>
2022-12-12 08:00:44 +0900

    Release v1.0.0-stable

--- index.js
+++ index.js
@@ -14,21 +14,36 @@ client.on("messageUpdate", (oldMessage, newMessage) => {
     if (oldMessage.author.id === client.user.id && newMessage.embeds[0] === undefined) {
         oldMessage.edit({embeds: [oldMessage.embeds[0]]});
     }
+    //TODO メッセージ編集検知機能追加
+    else {
+        if (oldMessage.author.avatarURL()) {
+            embed.setAuthor({name: `${oldMessage.author.username}`, iconURL: `${oldMessage.author.avatarURL()}`});
+        } else if (oldMessage.author.avatarURL() === null) {
+            embed.setAuthor({
+                name: `${oldMessage.author.username}`, iconURL: "https://cdn.discordapp.com/embed/avatars/1.png"
+            });
+        } else {
+            console.log("アバターアイコンで例外が発生しました");
+        }
+    }
 });
 
 client.on("messageDelete", message => {
     if (message.author.avatarURL()) {
         embed.setAuthor({name: `${message.author.username}`, iconURL: `${message.author.avatarURL()}`});
     } else if (message.author.avatarURL() === null) {
-        embed.setAuthor({name: `${message.author.username}`, iconURL: "https://cdn.discordapp.com/embed/avatars/1.png"})
+        embed.setAuthor({
+            name: `${message.author.username}`, iconURL: "https://cdn.discordapp.com/embed/avatars/1.png"
+        });
     } else {
         console.log("アバターアイコンで例外が発生しました");
     }
     if (message.author.id === client.user.id) {
-        const r_embed = message.embeds[0];
-        message.channel.send({content: "**メッセージが削除されました**", embeds: [r_embed]});
+        message.channel.send({content: "**メッセージが削除されました**", embeds: [message.embeds[0]]});
+    } else if (message.author.bot) {
         return;
     }
+    //TODO 添付ファイルの処理
     if (message.content) {
         embed.setDescription(`${message.content}`);
         if (message.attachments.size) {
@@ -41,5 +56,10 @@ client.on("messageDelete", message => {
     }
     message.channel.send({content: "**メッセージが削除されました**", embeds: [embed]});
 });
+
+
+//TODO スラッシュコマンド類
+//TODO スクリーンショット機能
+//TODO コンフィグ類
 
 client.login(process.env.TOKEN);
